e8c86c9ec5bea8c3dc222d97eef23e9437b136ca
Vadim Borisov <vborisov@nedra.digital>
2024-04-19 13:16:17 +0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Binary_Ones_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
